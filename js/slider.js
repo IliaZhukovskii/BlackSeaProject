196a60a1a0a8e5efdc8f6d1edd48c54a76538543
IliaZhukovskii <60235505+IliaZhukovskii@users.noreply.github.com>
2020-01-29 17:35:06 +0300

    first commit

---
+++ js/slider.js
@@ -0,0 +1,14 @@
+$('.slider').slick({
+    speed: 700,
+    slidesToShow: 1,
+    arrows:false,
+    dots: true,
+    draggable:true
+  });
+  
+
+
+$( ".nav__btn" ).click(function() {
+  $( ".menu" ).slideToggle( "slow", function() {
+});
+});
